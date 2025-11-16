8e2b01ff3220420b833d9806ccf0686fa591e47c
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-11-16 10:11:05 +0700

    fix: edit contact

--- app/components/MainPage/Banner.tsx
+++ app/components/MainPage/Banner.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const BannerInfoPage = () => {
   return (
-    <div className='w-full py-8 md:py-16 px-6 md:px-8 font-sans max-w-7xl mx-auto rounded-xl overflow-hidden'>
-        <section className="flex relative min-h-full w-full items-center">
-          <div className="flex flex-col z-20 px-6 py-32 text-white w-full max-w-7xl mx-auto">
-            <h1 className="text-start text-base">ระแนง Amigo</h1>
-            <h1 className="text-start text-3xl font-semibold">หมดปัญหาเรื่องปลวก น้ำ หรือ เชื้อรา</h1>
-            <p className="my-2">ระบบฝ้าระแนง Amigo ไม่มีปัญหาเรื่องปลวก ไม่ชวมน้ำ ไม่บิดโก่งงอ ไม่ขึ้นรา</p>
-            <Link href="#" className="bg-white text-black px-4 py-2 rounded-full w-fit">
-              ดูรายละเอียด
-            </Link>
+    <div className="w-full py-8 md:py-16 px-6 md:px-8 font-sans max-w-7xl mx-auto rounded-xl overflow-hidden">
+      <section className="flex relative min-h-full w-full items-center">
+        <div className="flex flex-col z-20 items-center px-6 py-48 text-white w-full max-w-7xl mx-auto gap-4">
+          <div className="flex flex-col gap-2 items-center">
+            <h1 className="text-center md:text-start text-4xl font-semibold">
+              สอบถาม ปรึกษา ฟรี
+            </h1>
+            <p>กรุณากรอกข้อมูลให้ครบถ้วน</p>
           </div>
-          <img src="https://iconroof.co.th/_next/image?url=%2Fasset%2Famigo.png&w=3840&q=75" alt="Product 1" className="absolute w-full h-full object-cover brightness-65 z-10 rounded-xl" />
-        </section>
+          <Link
+            href="#"
+            className="bg-white text-black px-4 py-2 rounded-full w-fit"
+          >
+            ติดต่อผ่านไลน์
+          </Link>
+        </div>
+        <img
+          src="https://iconroof.co.th/_next/image?url=%2Fasset%2Famigo.png&w=3840&q=75"
+          alt="Product 1"
+          className="absolute w-full h-full object-cover brightness-65 z-10 rounded-xl"
+        />
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default BannerInfoPage
+export default BannerInfoPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import Image from "next/image";
-import { ChevronDown, MessageCircleQuestion, ArrowUpRight, Pointer } from "lucide-react";
+import {
+  ChevronDown,
+  MessageCircleQuestion,
+  ArrowUpRight,
+  Pointer,
+} from "lucide-react";
 import Menutab from "./components/Menutab";
 import TestimonialSlider from "./components/TestimonialSlider";
 import Navbar from "./components/Navbar";
@@ -14,32 +19,34 @@ export default function Home() {
   const structuredData = {
     "@context": "https://schema.org",
     "@type": "LocalBusiness",
-    "name": "Iconroof",
-    "description": "จำหน่ายและติดตั้งระแนงบังตาไวนิล AMIGO ระแนงชิด ระแนงเว้นร่อง ระแนงบานเกล็ด",
-    "url": "https://iconroof.co.th/",
-    "telephone": "+66-XX-XXX-XXXX",
-    "address": {
+    name: "Iconroof",
+    description:
+      "จำหน่ายและติดตั้งระแนงบังตาไวนิล AMIGO ระแนงชิด ระแนงเว้นร่อง ระแนงบานเกล็ด",
+    url: "https://iconroof.co.th/",
+    telephone: "+66-XX-XXX-XXXX",
+    address: {
       "@type": "PostalAddress",
-      "addressCountry": "TH",
-      "addressLocality": "Thailand"
+      addressCountry: "TH",
+      addressLocality: "Thailand",
     },
-    "image": "https://tvss01.iconroof.co.th/e6c6f6e8-e73e-40b1-9f05-5436e5f99137/asset/og-img.jpg",
-    "priceRange": "$$",
-    "openingHoursSpecification": [
+    image:
+      "https://tvss01.iconroof.co.th/e6c6f6e8-e73e-40b1-9f05-5436e5f99137/asset/og-img.jpg",
+    priceRange: "$$",
+    openingHoursSpecification: [
       {
         "@type": "OpeningHoursSpecification",
-        "dayOfWeek": [
+        dayOfWeek: [
           "Monday",
           "Tuesday",
           "Wednesday",
           "Thursday",
           "Friday",
-          "Saturday"
+          "Saturday",
         ],
-        "opens": "08:00",
-        "closes": "17:30"
-      }
-    ]
+        opens: "08:00",
+        closes: "17:30",
+      },
+    ],
   };
 
   return (
@@ -52,41 +59,52 @@ export default function Home() {
         <section className="flex relative min-h-screen min-w-screen items-end -mt-16 md:-mt-20">
           <div className="flex flex-col z-20 p-6 text-white md:max-w-7xl lg:mx-auto w-full lg:mb-4">
             <h1 className="text-start text-base">สินค้าใหม่</h1>
-            <h1 className="text-start text-3xl font-semibold">ระแนงไวนิล วีจีเวนโต้</h1>
-            <p className="my-2">บริการลูกค้าการนำเข้าธุรกิจค้าส่งมอบองค์กรการบรรลุเป้าหมายโทรบัญชีงบประมาณ</p>
-            <Link href="#" className="bg-white text-black px-4 py-2 rounded-full w-fit">
+            <h1 className="text-start text-3xl font-semibold">
+              ระแนงไวนิล วีจีเวนโต้
+            </h1>
+            <p className="my-2">
+              บริการลูกค้าการนำเข้าธุรกิจค้าส่งมอบองค์กรการบรรลุเป้าหมายโทรบัญชีงบประมาณ
+            </p>
+            <Link
+              href="#"
+              className="bg-white text-black px-4 py-2 rounded-full w-fit"
+            >
               ดูรายละเอียด
             </Link>
           </div>
-          <img src="https://vg-cnp.com/wp-content/uploads/2025/06/2025-M04-Leaflet-DL-Slat-TH_%E0%B9%84%E0%B8%9F%E0%B8%A5%E0%B9%8C%E0%B8%9C%E0%B8%A5%E0%B8%B4%E0%B8%95-01.jpg" alt="Product 1" className="absolute w-full h-screen object-cover brightness-65 z-10" />
+          <img
+            src="https://vg-cnp.com/wp-content/uploads/2025/06/2025-M04-Leaflet-DL-Slat-TH_%E0%B9%84%E0%B8%9F%E0%B8%A5%E0%B9%8C%E0%B8%9C%E0%B8%A5%E0%B8%B4%E0%B8%95-01.jpg"
+            alt="Product 1"
+            className="absolute w-full h-screen object-cover brightness-65 z-10"
+          />
         </section>
-      <Menutab />
+        <Menutab />
 
-      <ProductInfoPage />
+        <ProductInfoPage />
 
-      <BrandListPage />
+        <BrandListPage />
 
-      <TestimonialSlider />
+        <TestimonialSlider />
 
-      <LatestArticlePage />
+        <LatestArticlePage />
 
-      <ContactInfoPage />
-
-      <section className="flex flex-col items-start justify-center mt-4 min-w-full py-2 px-4">
-        <MessageCircleQuestion className="h-12 w-12 mb-4 text-[#d9300c]" />
-        <h1 className="text-start text-3xl text-[#d9300c] font-semibold">ติดต่อปรึกษาฟรี!</h1>
-        <p className="mt-2 mb-4">หากสนใจแต่ไม่รู้จะเริ่มอย่างไรดี ดูที่นี่</p>
-        <div className="flex flex-col gap-2 w-full">
-          <div className="flex justify-between p-2 border-b border-neutral-200">
-            <p>หากสนใจสั่งซื้อสินค้าต้องทำอย่างไร</p>
-            <ChevronDown className="self-center text-neutral-500" />
+        <section className="flex flex-col items-start justify-center mt-4 min-w-full py-2 px-4">
+          <MessageCircleQuestion className="h-12 w-12 mb-4 text-[#d9300c]" />
+          <h1 className="text-start text-3xl text-[#d9300c] font-semibold">
+            ติดต่อปรึกษาฟรี!
+          </h1>
+          <p className="mt-2 mb-4">หากสนใจแต่ไม่รู้จะเริ่มอย่างไรดี ดูที่นี่</p>
+          <div className="flex flex-col gap-2 w-full">
+            <div className="flex justify-between p-2 border-b border-neutral-200">
+              <p>หากสนใจสั่งซื้อสินค้าต้องทำอย่างไร</p>
+              <ChevronDown className="self-center text-neutral-500" />
+            </div>
+            <div className="flex justify-between p-2 border-b border-neutral-200">
+              <p>หากสนใจสั่งซื้อสินค้าต้องทำอย่างไร</p>
+              <ChevronDown className="self-center text-neutral-500" />
+            </div>
           </div>
-          <div className="flex justify-between p-2 border-b border-neutral-200">
-            <p>หากสนใจสั่งซื้อสินค้าต้องทำอย่างไร</p>
-            <ChevronDown className="self-center text-neutral-500" />
-          </div>
-        </div>
-      </section>
+        </section>
       </div>
 
       <BannerInfoPage />
