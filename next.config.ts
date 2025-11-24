d58697edb46593d7919f3d77f4586aa670faa8e8
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-11-24 17:33:22 +0700

    allows cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   assetPrefix: 'https://beta.iconroof.co.th',
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes
+        source: "/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Allow all origins (or specify https://iconroof.co.th)
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
